79097b161eba212fedd86ea089693523e4f38c55
satyaki-das-usc <satyakid@usc.edu>
2024-06-19 18:06:21 -0700

    added commands for windows

--- run.sh
+++ run.sh
@@ -10,6 +10,14 @@ PIP_CACHE_DIR=/media/satyaki/22cce3a2-7e84-4401-9b92-07e2fbeec5561/.cache/pip TM
 PIP_CACHE_DIR=/media/satyaki/22cce3a2-7e84-4401-9b92-07e2fbeec5561/.cache/pip TMPDIR=/media/satyaki/22cce3a2-7e84-4401-9b92-07e2fbeec5561/pip_tmp pip install --upgrade jupyter ipywidgets
 
 
+# For Unix
+
 PYTHONPATH="." python src/feature_detection.py
 PYTHONPATH="." python src/ignore_list_feature_detection.py
 PYTHONPATH="." python src/targeted_perturbation.py
+
+# For Windows
+$env:PYTHONPATH = "."
+python src/feature_detection.py
+python src/ignore_list_feature_detection.py
+python src/targeted_perturbation.py
